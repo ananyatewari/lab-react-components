7adcbe8995aaf1068f65797a8c60fa5aadd3cca7
ananyatewari <ananyatewari0205@gmail.com>
2024-01-13 10:43:32 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
 import './App.css';
-import lake from "./image/lake.JPG";
 import GallaryFooter from './components/GallaryFooter';
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-const imageData = [
-  {
-    id:1,
-    img:lake 
-  },
-  {
-    id:2,
-    img:lake
-  },
-  {
-    id:3,
-    img:lake
-  },
-  {
-    id:4,
-    img:lake
-  }
-]
+import imageData from './components/Data';
 
 function App() {
   return (
